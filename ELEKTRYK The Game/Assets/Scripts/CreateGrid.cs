1eb7410c6d862dd7b83fde0f0272fe92bea4161c
DyyziO <60621867+DyyziO@users.noreply.github.com>
2020-09-14 19:12:45 +0200

    Grid

---
+++ ELEKTRYK The Game/Assets/Scripts/CreateGrid.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGrid : MonoBehaviour {
+
+    [SerializeField] int width = 100;
+    [SerializeField] int height = 100;
+    [SerializeField] float cellSize = 1f;
+
+    private Grid grid;
+
+    private void Start() {
+        grid = new Grid(width, height, cellSize, new Vector3(0, 0));
+    }
+}
